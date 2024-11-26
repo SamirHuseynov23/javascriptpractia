408a592b4f6438ab6957cc078f32bd762c2f4809
Samir <samir.huseynov.2395@gmail.com>
2024-11-26 22:04:02 +0400

    game test
    game test

---
+++ js_game_paper_and_rock/app.js
@@ -0,0 +1,9 @@
+let addName="rock";
+
+
+let items=['rock','paper','scissors'];
+
+let randomItems=items[Math.floor(Math.random()*3)]
+
+console.log(randomItems==addName?`${randomItems}==${addName} you win`:`${randomItems}==${addName} you not win`)
+
